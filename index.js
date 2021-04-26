4b5fda9d100f7d25efe00e571519ba2ff1d71f86
Ian Darland <iansmbpro@ians-mbp.lan>
2021-04-25 23:56:11 -0500

    saving changes to root

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const connection = mysql.createConnection({
   user: 'root',
 
   // Be sure to update with your own MySQL password!
-  password: 'Lexi420',
+  password: 'root',
   database: 'emp_trackerDB',
 });
 
